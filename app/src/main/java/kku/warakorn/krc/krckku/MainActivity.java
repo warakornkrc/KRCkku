4a866669557376b1fe326963689b75365a7741c3
warakorn krc <drivegaim@mail.com>
2016-11-12 10:24:45 +0700

    Start App

---
+++ app/src/main/java/kku/warakorn/krc/krckku/MainActivity.java
@@ -0,0 +1,13 @@
+package kku.warakorn.krc.krckku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
