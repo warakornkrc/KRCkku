d8be290fe3daa8c68859437e59f46ca58458fd84
warakorn krc <drivegaim@mail.com>
2016-11-12 12:04:53 +0700

    intent to signUp

--- app/src/main/java/kku/warakorn/krc/krckku/MainActivity.java
+++ app/src/main/java/kku/warakorn/krc/krckku/MainActivity.java
@@ -1,13 +1,36 @@
 package kku.warakorn.krc.krckku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+        //Bline Widget
+        signInButton = (Button) findViewById(R.id.button2);
+        signUpButton = (Button) findViewById(R.id.button);
+
+        //sign Up Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+    }   // Main Method
+} //Main Class //นี่คือ คลาสหลัก
